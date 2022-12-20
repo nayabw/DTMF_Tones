ffd3528729f0ba59ce615ec78195fb47523179b7
Nayab W <42422512+nayabw@users.noreply.github.com>
2022-12-20 13:25:43 -0600

    Add the license disclaimer

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,21 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.LineUnavailableException;
 import java.io.ByteArrayInputStream;
 
+/*      A simple Java project to take a .txt file with a list of DTMF tone codes, present the names to the user, and "dial" them when selected.
+        Copyright (C) 2022 Nayab W.
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 public class Main {
 
